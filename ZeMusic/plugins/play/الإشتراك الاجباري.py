22c836b2a87ccbd670df90a380da9a2ba3f8e6cb
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-05-31 02:10:54 +0300

    Update الإشتراك الاجباري.py

--- ZeMusic/plugins/play/الإشتراك الاجباري.py
+++ ZeMusic/plugins/play/الإشتراك الاجباري.py
@@ -5,7 +5,7 @@ from pyrogram.enums import ChatType
 from pyrogram.errors import UserNotParticipant
 from ZeMusic import app
 
-channel = "K55DD"
+channel = "eo_u7"
 async def subscription(_, __: Client, message: Message):
     user_id = message.from_user.id
     try: await app.get_chat_member(channel, user_id)
